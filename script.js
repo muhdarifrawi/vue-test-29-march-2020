3a1e09fafa3c61c2046b5cb3c4d5ef4caed8b095
muhdarifrawi <muhdarifrawi@gmail.com>
2020-06-08 13:06:17 +0800

    Using onhover event to change product image

--- script.js
+++ script.js
@@ -6,12 +6,24 @@ var heroApp = new Vue ({
         image: "/images/green-socks.jpg",
         link: "https://www.google.com",
         onSale: true,
-        colours: ["red", "blue", "green"],
+        colours: [{
+            id:1001,
+            coloursChoice: "blue",
+            image:"/images/blue-socks.jpg"
+        },
+        {
+            id:1002,
+            coloursChoice: "green",
+            image:"/images/green-socks.jpg"
+        }],
         cart: 0
     },
     methods: {
         addToCart: function() {
             this.cart += 1;
+        },
+        updateProduct: function(image){
+            this.image = image;
         }
     }
 })
